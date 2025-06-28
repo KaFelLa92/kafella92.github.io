b565eac2beebe33d9f59308a8cc31d72a72b87e2
KaFelLa92 <choidj10@gmail.com>
2025-06-28 12:17:43 +0900

    asdf

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // node.js로 서버 생성하는 JS입니다.
+
+const express = require('express');
+const path = require('path');
+const static = require('serve-static');
+
+var app = express();
+app.use('/W', static(path.join(__dirname, 'web')));
+
+app.get('/', (req, res) => {
+    res.redirect(302, '/w/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('server start!!!');
+})
+
