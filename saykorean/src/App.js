5563758a8ab31b85f1bf871b4a1153cfa3355e5c
KaFelLa92 <choidj10@gmail.com>
2025-07-03 19:50:09 +0900

    asdf

--- saykorean/src/App.js
+++ saykorean/src/App.js
@@ -1,25 +1,33 @@
-function Greeting(props) { // 간단한 함수형 컴포넌트
-  return <h1> 안녕하세요, {props.name}님! </h1>;
-}
+import logo from './logo.svg';
+import './App.css';               // css import
+import { useState } from 'react'; // state import
 
-// 복잡한 예시
-function WelcomeMessage() {
-  const name = "SAYKOREAN";
-  const message = "한국어를 잘하려면 한국인처럼 생각하면 됩니다.";
 
-  return (
-    <div>
-      <h1>안녕하세요, 저는 {name}입니다.</h1>
-      <p>{message}</p>
-    </div>
-  )
-}
 
 function App() {
+
+  let post = '밥에 관련한 한국어 표현';
+  let [title, b] = useState('오홍홍햄빠끄 밥 한 번 먹자'); // state 문법 사용 방법
+  let [logo, setLogo] = useState('ReactNovice');
+
+  // 디스트럭처링 문법 시작
+  let num = [1, 2, 3];
+  let a = num[0];
+  let c = num[1];
+  console.log(c);     // 2가 나옴. 배열에서 찾아서 값을 넣어줌
+
+  let [d, e] = [1, 2];  // 형태 맞추면 대응해서 집어넣어짐
+  //디스트럭처링 문법 끝
+
   return (
-    <div>
-      <Greeting></Greeting>
-      <WelcomeMessage></WelcomeMessage>
+    <div className="App">
+      <div className="head-nav">
+        <h4> {logo} </h4>
+      </div>
+      <div className='list'>
+        <h4>{title}</h4>
+        <p>2월 17일 발행 </p>
+      </div>
     </div>
   );
 }
